4c5d0f42f8e73db4448756480c9a24f90fb69b65
jinimania <jjinimania@gmail.com>
2015-08-27 14:50:01 +0900

    Plus home method

--- src/main/java/demo/SpringbootDemoApplication.java
+++ src/main/java/demo/SpringbootDemoApplication.java
@@ -2,9 +2,17 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootDemoApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Spring Boot";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
